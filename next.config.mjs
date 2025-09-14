ebac126084794257c2fa93bd03d4f1ba847bc345
Artem Teslenko <ates@ipv6.dp.ua>
2025-09-14 12:16:20 +0300

    WIP

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        unoptimized: true
+    },
     output: 'export',
     devIndicators: {
       buildActivity: false, // disables the build spinner in the corner
