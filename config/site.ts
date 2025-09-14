e961b08bc7b22dc38fe944dedd9a82a52f586978
tdelic1982 <tarik@octa.space>
2025-09-15 00:02:05 +0200

    Meta update

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig = {
     "https://octa.space/",
   ogImage: "https://octa.space/og.jpg",
   description:
-    "Unleash the power of AI and cloud computing with OctaSpace, a decentralized GPU network. Experience high-performance, cost-effective, and secure computing for all your workloads.",
+    "Unleash the power of AI & cloud computing with OctaSpace, a decentralized GPU network. Experience high-performance, cost-effective, and secure computing for your workloads.",
   links: {
     twitter: "https://x.com/octa_space",
     github: "https://github.com/octaspace",
