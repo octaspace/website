67ff89dce0ff5d65838f635b6c9597ed5cc7bc7e
tdelic1982 <tarik@octa.space>
2025-09-14 12:17:29 +0200

    Site config

--- config/site.ts
+++ config/site.ts
@@ -2,30 +2,14 @@ export const siteConfig = {
   name: "OctaSpace | Decentralized compute",
   url: "https://octa.space/",
   getStartedUrl:
-    "https://launchuicomponents.com/docs/getting-started/introduction",
-  ogImage: "https://launchuicomponents.com/og.jpg",
+    "https://marketplace.octa.space/",
+  ogImage: "https://octa.space/og.jpg",
   description:
-    "Landing page components built with React, Shadcn/ui and Tailwind that will make your website feel premium.",
+    "OctaSpace AI & Cloud Compute. Powered by a Decentralized GPU Network.",
   links: {
     twitter: "https://x.com/octa_space",
     github: "https://github.com/octaspace",
     email: "mailto:hello@octa.space",
-  },
-  pricing: {
-    pro: "https://launchui.lemonsqueezy.com/buy/b4798c68-c858-4c34-860b-069b5a0d6c4e",
-    team: "https://launchui.lemonsqueezy.com/buy/130d8cfe-e123-464b-9f67-c74c5fedfb45",
-  },
-  stats: {
-    figma: 4352,
-    github: 1049,
-    cli: 8092,
-    total: "13.4k+",
-    updated: "23 Jul 2025",
-    sections: 72,
-    illustrations: 15,
-    animations: 14,
-    appTemplates: 2,
-    websiteTemplates: 4,
   },
 };
 
